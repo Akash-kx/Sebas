b4fc40ed72b7e6de18d28d3f6a1421bb6a349c1c
Akash Singh Rawat <129946078+Akash-kx@users.noreply.github.com>
2023-09-15 23:40:25 +0530

    Update SecurityConfiguration.java

--- Sebas/Sebas/src/main/java/com/Ecommerce/Sebas/SpringSecurityConfiguration/SecurityConfiguration.java
+++ Sebas/Sebas/src/main/java/com/Ecommerce/Sebas/SpringSecurityConfiguration/SecurityConfiguration.java
@@ -1,5 +1,8 @@
 package com.Ecommerce.Sebas.SpringSecurityConfiguration;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,8 +12,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import com.Ecommerce.Sebas.JWTtoken.JwtFilter;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 public class SecurityConfiguration {
@@ -26,17 +33,27 @@ public class SecurityConfiguration {
 		
 		String[] AccessAllocate = {"/css/**", "/js/**"};
 		
-		http.csrf().disable()
+		http.cors().configurationSource(new CorsConfigurationSource() {
+
+			@Override
+			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+				CorsConfiguration config= new CorsConfiguration();
+				config.setAllowCredentials(true);
+				config.setAllowedHeaders(Collections.singletonList("*"));
+				config.setAllowedMethods(Collections.singletonList("*"));
+				config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+				config.setMaxAge(3600L);
+				config.setExposedHeaders(Arrays.asList("NAME-X"));
+				return config;
+			}
+			
+		})
+		.and().csrf().disable()
 		.authorizeHttpRequests()
 		.requestMatchers(AccessAllocate).permitAll()
 		.requestMatchers("/AddProductInInventory", "/ShowAllProductInInventory", "/ShowSearchedProduct/**", "/ShowAllProductForMen/**", "/ShowAllProductForWomen/**", "/ShowAllSearchedProduct/**" ,"/ShowOnlySpecificProduct").permitAll()
 		.requestMatchers("/ShowRegistrationForm", "/UserRegistrationInformation").permitAll()
-		.requestMatchers("/AddProductInWishList", "/ShowAllProductInWishList" ,"/DeleteSpecificProductInWishList/**", "/AddProductInAddToBagFromWishList/**", "/AddProductInAddToBag").permitAll()
-		.requestMatchers("/ShowAllProductInAddToBag", "/DeleteSpecificProductInAddToBag/**", "/MoveProductToWishList/**", "/UpdateProductInAddToBag/**").permitAll()	
-		.requestMatchers("/DeleteSpecificProductInAddToBag/**", "/MoveProductToWishList/**", "/UpdateProductInAddToBag", "/ShowAllProductIdInAddToBag", "/MoveProductToYourOrder/**").permitAll()
-		.requestMatchers("/ShowAllProductInYourOrder", "/DeleteSpecificProductInYourOrder/**").permitAll()
 		.requestMatchers("/AddLoginInformation").permitAll()
-		.requestMatchers("/UserCardDetail").permitAll()
 		.anyRequest().authenticated()
 		.and().httpBasic()
 		.and().formLogin().loginPage("http://localhost:4200/LoginPage").usernameParameter("userEmail").usernameParameter("userPassword")
